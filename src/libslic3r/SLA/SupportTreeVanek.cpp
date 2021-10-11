ddd1f2e0eda21b59cab58a9a98e050863693bf19
tamasmeszaros <meszaros.q@gmail.com>
2021-10-11 15:24:08 +0200

    Get rid of boost rtree as it has no good use.

--- src/libslic3r/SLA/SupportTreeVanek.cpp
+++ src/libslic3r/SLA/SupportTreeVanek.cpp
@@ -6,7 +6,6 @@
 
 #include <igl/random_points_on_mesh.h>
 
-#include "SLA/SpatIndex.hpp"
 #include "SLA/SupportTreeBuildsteps.hpp"
 #include "libslic3r/Tesselate.hpp"
 
@@ -111,101 +110,120 @@ inline double merge_distance(const Vec3f &a, const Vec3f &b, double bridge_slope
     return ret;
 };
 
+enum PtType { SUPP, MESH, BED, JUNCTION, NONE };
+
+class PointCloud {
+    const std::vector<Junction> &m_roots;
+    std::vector<Junction> m_junctions, m_meshpoints, m_bedpoints;
+
+    const double bridge_slope;
+
+    const size_t I1, I2, I3;
+
+    std::vector<bool> m_searchable_indices;
+    size_t m_reachable_cnt;
+
+public:
+    PointCloud(const indexed_triangle_set &M,
+               const std::vector<Junction> &support_roots,
+               const Properties &          props)
+        : m_roots{support_roots}
+        , m_meshpoints{sample_mesh(M)}
+        , m_bedpoints{sample_bed(props.bed_shape(), props.ground_level())}
+        , bridge_slope{props.max_slope()}
+        , I1{m_roots.size()}
+        , I2{I1 + m_meshpoints.size()}
+        , I3{I2 + m_bedpoints.size()}
+        , m_searchable_indices(I3, true)
+        , m_reachable_cnt{I3}
+    {
+    }
+
+    PtType get_type(size_t node_id) const
+    {
+        PtType ret = NONE;
+
+        if (node_id < I1) ret = SUPP;
+        else if (node_id < I2) ret = MESH;
+        else if (node_id < I3) ret = BED;
+        else if (node_id < I3 + m_junctions.size()) ret = JUNCTION;
+
+        return ret;
+    }
+
+    Junction get(size_t node_id) const
+    {
+        switch(get_type(node_id)) {
+        case SUPP: return m_roots[node_id];
+        case MESH: return m_meshpoints[node_id - I1];
+        case BED:  return m_bedpoints [node_id - I2];
+        case JUNCTION: return m_junctions[node_id - I3];
+        case NONE: ;
+        }
+
+        return Vec3f{};
+    }
+
+    Vec3f get_coord(size_t node_id) const { return get(node_id).pos; }
+
+    double get_distance(const Vec3f &p, size_t node)
+    {
+        auto t = get_type(node);
+
+        switch (t) {
+        case MESH:
+        case BED: {
+            // Points of mesh or bed which are outside of the support cone of
+            // 'pos' must be discarded.
+            Vec3f D = get_coord(node) - p;
+            double Ddist = D.norm();
+            double polar = std::acos(D.z() / Ddist);
+            if (polar < PI - bridge_slope)
+                return std::numeric_limits<double>::infinity();
+            else
+                return Ddist;
+        }
+        case SUPP:
+        case JUNCTION:
+            return merge_distance(p, get_coord(node), bridge_slope);
+        case NONE:
+            ;
+        }
+        return std::numeric_limits<double>::infinity();
+    }
+
+    size_t insert_junction(const Junction &p)
+    {
+        size_t new_id = I3 + m_junctions.size();
+        m_junctions.emplace_back(p);
+        m_searchable_indices.emplace_back(true);
+        ++m_reachable_cnt;
+
+        return new_id;
+    }
+
+    void remove_node(size_t node_id)
+    {
+        m_searchable_indices[node_id] = false;
+        --m_reachable_cnt;
+    }
+
+    size_t reachable_count() const { return m_reachable_cnt; }
+
+    template<class Fn> void foreach_reachable(const Vec3f &pos, Fn &&visitor)
+    {
+        for (size_t i = 0; i < m_searchable_indices.size(); ++i)
+            if (m_searchable_indices[i])
+                visitor(i, get_distance(pos, i));
+    }
+};
+
 bool build_tree(const indexed_triangle_set & its,
                 const std::vector<Junction> &support_roots,
                 Builder &                    builder,
                 const Properties &           properties)
 {
-    enum PtType { SUPP, MESH, BED, JUNCTION, NONE };
-
-    struct PointCloud {
-        const std::vector<Junction> &supppts;
-        std::vector<Junction> junctions, meshpts, bedpts;
-        sla::PointfIndex spatindex;
-        const double bridge_slope;
-
-        const size_t I1, I2, I3;
-
-        PointCloud(const indexed_triangle_set &M,
-                   const std::vector<Junction> &support_roots,
-                   const Properties &          props)
-            : supppts{support_roots}
-            , meshpts{sample_mesh(M)}
-            , bedpts{sample_bed(props.bed_shape(), props.ground_level())}
-            , bridge_slope{props.max_slope()}
-            , I1{supppts.size()}
-            , I2{I1 + meshpts.size()}
-            , I3{I2 + bedpts.size()}
-        {
-            for (size_t i = 0; i < supppts.size(); ++i)
-                spatindex.insert(std::make_pair(supppts[i].pos, i));
-            for (size_t i = 0; i < meshpts.size(); ++i)
-                spatindex.insert(std::make_pair(meshpts[i].pos, I1 + i));
-            for (size_t i = 0; i < bedpts.size(); ++i)
-                spatindex.insert(std::make_pair(bedpts[i].pos, I2 + i));
-        }
-
-        PtType get_type(size_t i) const
-        {
-            PtType ret = NONE;
-
-            if (i < I1) ret = SUPP;
-            else if (i < I2) ret = MESH;
-            else if (i < I3) ret = BED;
-            else if (i < I3 + junctions.size()) ret = JUNCTION;
-
-            return ret;
-        }
-
-        Junction get(size_t i) const
-        {
-            switch(get_type(i)) {
-            case SUPP: return supppts[i];
-            case MESH: return meshpts[i - I1];
-            case BED:  return bedpts [i - I2];
-            case JUNCTION: return junctions[i - I3];
-            case NONE: ;
-            }
-
-            return Vec3f{};
-        }
-
-        Vec3f get_coord(size_t i) const { return get(i).pos; }
-
-        double get_distance(const Vec3f &p, const PointfIndexEl &el)
-        {
-            auto t = get_type(el.second);
-
-            switch (t) {
-            case MESH:
-            case BED: {
-                // Points of mesh or bed which are outside of the support cone of
-                // 'pos' must be discarded.
-                Vec3f D = el.first - p;
-                double Ddist = D.norm();
-                double polar = std::acos(D.z() / Ddist);
-                if (polar < PI - bridge_slope)
-                    return std::numeric_limits<double>::infinity();
-                else
-                    return Ddist;
-            }
-            case SUPP:
-            case JUNCTION:
-                return merge_distance(p, el.first, bridge_slope);
-            case NONE:
-                ;
-            }
-            return std::numeric_limits<double>::infinity();
-        }
-
-        size_t insert_junction(const Junction &p)
-        {
-            size_t new_idx = I3 + junctions.size();
-            junctions.emplace_back(p);
-            spatindex.insert(std::make_pair(p.pos, new_idx));
-            return new_idx;
-        }
-    } nodes(its, support_roots, properties);
+    PointCloud nodes(its, support_roots, properties);
 
     std::deque<size_t> ptsqueue(support_roots.size());
     std::iota(ptsqueue.begin(), ptsqueue.end(), 0);
@@ -217,33 +235,32 @@ bool build_tree(const indexed_triangle_set & its,
     std::sort(ptsqueue.begin(), ptsqueue.end(), zcmp);
 
     while (!ptsqueue.empty()) {
-        size_t idx = ptsqueue.back();
+        size_t node_id = ptsqueue.back();
         ptsqueue.pop_back();
 
-        Junction node = nodes.get(idx);
-        nodes.spatindex.remove(std::make_pair(node.pos, idx));
+        Junction node = nodes.get(node_id);
+        nodes.remove_node(node_id);
 
-        auto res  = nodes.spatindex.query(node.pos, nodes.spatindex.size());
+        struct NodeDistance { size_t node_id; double distance; };
+        auto distances = reserve_vector<NodeDistance>(nodes.reachable_count());
 
-        std::vector<std::pair<size_t, double>>
-            distances(res.size(), std::make_pair(0, std::nan("")));
-
-        for (size_t i = 0; i < res.size(); ++i)
-            distances[i] = std::make_pair(i, nodes.get_distance(node.pos, res[i]));
-
-        std::sort(distances.begin(), distances.end(), [](auto &a, auto &b){
-            return a.second < b.second;
+        nodes.foreach_reachable(node.pos, [&distances](size_t id, double distance) {
+            distances.emplace_back(NodeDistance{id, distance});
         });
 
-        if (std::isinf(distances[0].second)) {
-            builder.report_unroutable(idx);
+        std::sort(distances.begin(), distances.end(), [](auto &a, auto &b){
+            return a.distance < b.distance;
+        });
+
+        if (distances.empty() || std::isinf(distances.front().distance)) {
+            builder.report_unroutable(node_id);
             continue;
         }
 
-        PointfIndexEl closest = res[distances[0].first];
-        Junction closest_node = nodes.get(closest.second);
+        size_t closest_node_id = distances.front().node_id;
+        Junction closest_node = nodes.get(closest_node_id);
 
-        auto type = nodes.get_type(closest.second);
+        auto type = nodes.get_type(distances.front().node_id);
         closest_node.R = node.R;
 
         switch (type) {
@@ -257,23 +274,23 @@ bool build_tree(const indexed_triangle_set & its,
         }
         case SUPP:
         case JUNCTION: {
-            auto mergept = find_merge_pt(node.pos, closest.first, properties.max_slope());
+            auto mergept = find_merge_pt(node.pos, closest_node.pos, properties.max_slope());
             if (!mergept) continue;
 
             Junction mergenode{*mergept, node.R};
 
-            if ((*mergept - closest.first).norm() > EPSILON) {
+            if ((*mergept - closest_node.pos).norm() > EPSILON) {
                 size_t new_idx = nodes.insert_junction({*mergept, node.R});
                 auto it = std::lower_bound(ptsqueue.begin(), ptsqueue.end(), new_idx, zcmp);
                 ptsqueue.insert(it, new_idx);
 
                 // Remove the connected support point from the queue
-                it = std::lower_bound(ptsqueue.begin(), ptsqueue.end(), closest.second, zcmp);
+                it = std::lower_bound(ptsqueue.begin(), ptsqueue.end(), closest_node_id, zcmp);
                 if (it != ptsqueue.end()) {
                     ptsqueue.erase(it);
                 }
 
-                nodes.spatindex.remove(closest);
+                nodes.remove_node(closest_node_id);
                 builder.add_bridge(closest_node, mergenode);
             }
 
